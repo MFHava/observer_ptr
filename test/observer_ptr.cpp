884af7eda4195e211f37a919e2e613f8cc4ac522
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2019-07-22 14:58:59 +0200

    * fixed typo and added inc folder as include path * replaced weird dot chars by spaces

--- test/observer_ptr.cpp
+++ test/observer_ptr.cpp
@@ -1,11 +1,11 @@
 
-//··········Copyright·Bernhard Manfred Gruber & Michael·Florian·Hava.
-//·Distributed·under·the·Boost·Software·License,·Version·1.0.
-//····(See·accompanying·file·../LICENSE_1_0.txt·or·copy·at
-//··········http://www.boost.org/LICENSE_1_0.txt)
+//          Copyright Bernhard Manfred Gruber & Michael Florian Hava.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
-#include·<boost/test/unit_test.hpp>
-#include·"observer_ptr/observer_ptr.hpp"
+#include <boost/test/unit_test.hpp>
+#include "observer_ptr/observer_ptr.hpp"
 
 BOOST_AUTO_TEST_SUITE(observer_ptr)
 
