dba882b932f3f6f977cb2418379c768143749ca2
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2019-11-01 21:22:32 +0100

    removed some comments and added a few noexcept

--- include/boost/observer_ptr/observer_ptr.hpp
+++ include/boost/observer_ptr/observer_ptr.hpp
@@ -6,7 +6,7 @@
 //    (See accompanying file ../../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
-//  See http://www.boost.org/libs/observer_ptr/ for documentation. TODO
+//  See http://www.boost.org/libs/observer_ptr/ for documentation.
 
 #pragma once
 
@@ -15,7 +15,6 @@
 #include <type_traits>
 
 namespace boost {
-	//  TODO: documentation
 	template <typename T>
 	class observer_ptr {
 		using pointer = typename std::add_pointer<T>::type;
@@ -36,7 +35,7 @@ namespace boost {
 			return ptr;
 		}
 
-		BOOST_CXX14_CONSTEXPR reference operator*() const {//TODO noexcept here? AFAIK there is an exception to the Lakos Rule in the standard libraries smart pointers... (at least shared_ptr does)
+		BOOST_CXX14_CONSTEXPR reference operator*() const noexcept {
 			BOOST_ASSERT(ptr != nullptr);
 			return *get();
 		}
@@ -83,12 +82,12 @@ namespace boost {
 	}
 
 	template <typename T1, typename T2>
-	constexpr bool operator==(observer_ptr<T1> p1, observer_ptr<T2> p2) {
+	constexpr bool operator==(observer_ptr<T1> p1, observer_ptr<T2> p2) noexcept {
 		return p1.get() == p2.get();
 	}
 
 	template <typename T1, typename T2>
-	constexpr bool operator!=(observer_ptr<T1> p1, observer_ptr<T2> p2) {
+	constexpr bool operator!=(observer_ptr<T1> p1, observer_ptr<T2> p2) noexcept {
 		return !(p1 == p2);
 	}
 
@@ -114,24 +113,11 @@ namespace boost {
 
 	template <typename T1, typename T2>
 	constexpr bool operator<(observer_ptr<T1> p1, observer_ptr<T2> p2) noexcept {
-		// TODO: where W3 is the composite pointer type (C++17 §8) of T1* and T2*.
-		// from N4820:
-		//4 The composite pointer type of two operands p1 and p2 having types T1 and T2, respectively, where at least one is a pointer or pointer-to-member type or std::std::nullptr_t, is:
-		//(4.1) - if both p1 and p2 are null pointer constants, std::std::nullptr_t;
-		//(4.2) - if either p1 or p2 is a null pointer constant, T2 or T1, respectively;
-		//(4.3) - if T1 or T2 is "pointer to cv1 void" and the other type is "pointer to cv2 T", where T is an object type or void, "pointer to cv12 void", where cv12 is the union of cv1 and cv2;
-		//(4.4) - if T1 or T2 is "pointer to noexcept function" and the other type is "pointer to function", where the function types are otherwise the same, "pointer to function";
-		//(4.5) - if T1 is "pointer to cv1 C1" and T2 is "pointer to cv2 C2", where C1 is reference-related to C2 or C2 is reference-related to C1(9.3.3), the cv-combined type of T1 and T2 or the cv-combined type of T2 and T1, respectively;
-		//(4.6) - if T1 or T2 is "pointer to member of C1 of type function", the other type is "pointer to member of C2 of type noexcept function", and C1 is reference-related to C1 or C2 is reference-related to C1(9.3.3), where the function types are otherwise the same, "pointer to member of C2 of type function" or "pointer to member of C1 of type function", respectively;
-		//(4.7) - if T1 is "pointer to member of C1 of type cv1 U" and T2 is "pointer to member of C2 of type cv2 U", for some non-function type U, where C1 is reference-related to C2 or C2 is reference-related to C1(9.3.3), the cv-combined type of T2 and T1 or the cv-combined type of T1 and T2, respectively;
-		//(4.8) - if T1 and T2 are similar types(7.3.5), the cv-combined type of T1 and T2;
-		//(4.9) - otherwise, a program that necessitates the determination of a composite pointer type is ill-formed.
-
-		using W3 = typename std::common_type<
+		using T3 = typename std::common_type<
 			typename std::add_pointer<T1>::type,
 			typename std::add_pointer<T2>::type
 		>::type;
-		return std::less<W3>{}(p1.get(), p2.get());
+		return std::less<T3>{}(p1.get(), p2.get());
 	}
 
 	template <typename T1, typename T2>
